ea956dd4ff864b89c5fde2aeefcfa4efa0180024
Tarun Kumar <tarunkumarpk33@gmail.com>
2022-08-04 16:21:52 +0530

    dom first comment

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log(document);
+
+console.dir(document.firstChild);
+console.log(document.getElementById('header-title'));
+let h1=document.getElementById('header-title');
+console.log(h1.innerText);
+console.log(h1.textContent);
+h1.style.fontWeight="bold";
+h1.style.color="green";
+h1=document.getElementById('main-header');
+
+h1.style.borderBottom="2px solid black";
