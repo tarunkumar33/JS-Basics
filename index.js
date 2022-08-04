eb333dc0fd85a25a6c2d2181b11fc3e5b1390cd6
Tarun Kumar <tarunkumarpk33@gmail.com>
2022-08-04 16:48:15 +0530

    second DOM commit

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ console.log(document.getElementById('header-title'));
 let h1=document.getElementById('header-title');
 console.log(h1.innerText);
 console.log(h1.textContent);
-h1.style.fontWeight="bold";
-h1.style.color="green";
+
 h1=document.getElementById('main-header');
 
 h1.style.borderBottom="2px solid black";
+
+h1=document.querySelector('.title');
+h1.style.fontWeight="bold";
+h1.style.color="green";
+console.log(h1);
