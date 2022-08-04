42da99c15d3d3aae3b13f168c40b7f71ddd1d9f5
Tarun Kumar <tarunkumarpk33@gmail.com>
2022-08-04 18:39:33 +0530

    Task 7 basics

--- index.js
+++ index.js
@@ -1,43 +1,82 @@
-console.log(document);
+// console.log(document);
 
-console.dir(document.firstChild);
-console.log(document.getElementById('header-title'));
-let h1=document.getElementById('header-title');
-console.log(h1.innerText);
-console.log(h1.textContent);
+// console.dir(document.firstChild);
+// console.log(document.getElementById('header-title'));
+// let h1=document.getElementById('header-title');
+// console.log(h1.innerText);
+// console.log(h1.textContent);
 
-h1=document.getElementById('main-header');
+// h1=document.getElementById('main-header');
 
-h1.style.borderBottom="2px solid black";
+// h1.style.borderBottom="2px solid black";
 
-h1=document.querySelector('.title');
-h1.style.fontWeight="bold";
-h1.style.color="green";
+// h1=document.querySelector('.title');
+// h1.style.fontWeight="bold";
+// h1.style.color="green";
+// console.log(h1);
+
+// let select;
+// // get elements by class name
+// select=document.getElementsByClassName('list-group-item');
+// select[2].style.backgroundColor="green";
+// for(let i=0;i<select.length;i++){
+//     select[i].style.fontWeight="bold";
+// }
+
+// //Tag name
+// select=document.getElementsByTagName('li');
+// select[2].style.backgroundColor="green";
+// for(let i=0;i<select.length;i++){
+//     select[i].style.fontWeight="bold";
+// }
+// //queryselector
+// select=document.querySelector('.list-group-item:nth-child(2)');
+// select.style.backgroundColor="green";
+
+// select=document.querySelector('.list-group-item:nth-child(3)');
+// select.style.display="none";
+
+// //queryselectorall
+// select=document.querySelectorAll('.list-group-item:nth-child(odd)');
+// for(let i=0;i<select.length;i++){
+//     select[i].style.backgroundColor="green";
+// }
+//Traversing the DOM
+select=document.querySelector('#items');
+console.log(select.parentNode);
+console.log(select.parentElement);
+console.log(select.children);
+console.log(select.firstElementChild);
+console.log(select.lastElementChild);
+console.log(select.nextElementSibling);
+console.log(select.previousElementSibling);
+console.log(select.childNodes);
+console.log(select.firstChild);
+console.log(select.lastChild);
+console.log(select.nextSibling);
+console.log(select.previousSibling);
+
+let h1=document.createElement('h1');
+h1.className="hello"
+h1.setAttribute('title','hello');
+h1.appendChild(document.createTextNode('HEllo'));
 console.log(h1);
 
-let select;
-// get elements by class name
-select=document.getElementsByClassName('list-group-item');
-select[2].style.backgroundColor="green";
-for(let i=0;i<select.length;i++){
-    select[i].style.fontWeight="bold";
-}
+let container=document.querySelector('header .container');
+let header=document.querySelector('header h1');
 
-//Tag name
-select=document.getElementsByTagName('li');
-select[2].style.backgroundColor="green";
-for(let i=0;i<select.length;i++){
-    select[i].style.fontWeight="bold";
-}
-//queryselector
-select=document.querySelector('.list-group-item:nth-child(2)');
-select.style.backgroundColor="green";
+container.insertBefore(h1,header);
+container=document.querySelector('#items');
+header=document.querySelector('.list-group-item');
 
-select=document.querySelector('.list-group-item:nth-child(3)');
-select.style.display="none";
+h1=document.createElement('h1');
+h1.className="hello"
+h1.setAttribute('title','hello');
+h1.appendChild(document.createTextNode('HEllo'));
+container.insertBefore(h1,header);
 
-//queryselectorall
-select=document.querySelectorAll('.list-group-item:nth-child(odd)');
-for(let i=0;i<select.length;i++){
-    select[i].style.backgroundColor="green";
-}
+
+
+
+
+
