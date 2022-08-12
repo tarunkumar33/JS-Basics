35f20b03e0602963a4795a5a11aade84dfd2235e
Tarun Kumar <tarunkumarpk33@gmail.com>
2022-08-12 23:51:09 +0530

    Task 12 AJS

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-var multiply=function(x,y){
-    console.log(x*y);
-}
-
-var multiplyby2=multiply.bind(this,2);
-
-multiplyby2(3);
-
-var multiply=function(x){
-    return function(y){
-        console.log(x*y);
+class student{
+    static c=0;
+    constructor(name,age,phno,marks){
+        student.c+=1;
+        this.name=name;
+        this.age=age;
+        this.phno=phno;
+        this.marks=marks;
+    }
+    setPlacementage(age){
+        return (marks)=>{
+            if(this.marks>marks && this.age>age)
+            {
+            console.log("is ready for placement");
+            return true;}
+            else{
+            console.log("not ready for placement");
+            return false;}
+        }
     }
 }
 
-var multiplyby2=multiply(2);
-multiplyby2(4);
+var obj=new student("a",20,12,80);
+obj.setPlacementage(10)(50);
+
+var a=[new student("abc",20,12,40),new student("a",20,12,80)];
+
+for(let i of a){
+    if(i.setPlacementage(10)(50)){
+        console.log(i.name);
+    }
+}
