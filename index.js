5ee24962582d7603ca60ff03cc03d67deef389cf
Tarun Kumar <tarunkumarpk33@gmail.com>
2022-08-20 14:07:00 +0530

    task 13 appoint delete

--- index.js
+++ index.js
@@ -29,45 +29,15 @@ function onSubmit(e) {
     // Remove error after 3 seconds
     setTimeout(() => msg.remove(), 3000);
   } else {
-    // Create new list item with user
-    var li = document.createElement('li');
-    li.className="list-group-item";
-    li.id=emailInput.value;
-    // Add text node with input values
-    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
-    var btn=document.createElement('button');
-    btn.className="btn btn-danger btn-sm float-right delete";
-    btn.appendChild(document.createTextNode('X'));
 
         var obj={
             name:nameInput.value,
             email:emailInput.value
         };
-        var x=emailInput.value;
-        // if(localStorage.getItem(x)){
-        //     var del=document.getElementById(x);
-        //     del.remove();
-        // }
-        // localStorage.setItem(emailInput.value,JSON.stringify(obj));
+
         axiosobj.post("/appointmentData",obj)
-        .then((res)=>console.log(res))
+        .then((res)=>showScreen(res.data))
         .catch(err=>console.log(err));
-
-
-    
-
-    // Add HTML
-    // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
-
-    // Append to ul
-    li.appendChild(btn);
-    btn=document.createElement('button');
-    btn.className="btn btn-primary btn-sm float-right edit";
-    btn.appendChild(document.createTextNode('-'));
-    li.appendChild(btn);
-    userList.appendChild(li);
-    
-
     // Clear fields
     nameInput.value = '';
     emailInput.value = '';
@@ -76,7 +46,7 @@ function onSubmit(e) {
 function showScreen(user){
     var li = document.createElement('li');
     li.className="list-group-item";
-    li.id=user.email;
+    li.id=user._id;
     li.appendChild(document.createTextNode(`${user.name}: ${user.email}`));
     var btn=document.createElement('button');
     btn.className="btn btn-danger btn-sm float-right delete";
@@ -97,39 +67,15 @@ axiosobj.get("/appointmentData")
     }
 })
 .catch(err=>console.log(err));
-Object.keys(localStorage).forEach((key) => {
-
-
-
-    stringifiedDetailsOfPeople = localStorage.getItem(key);
-    
-    user = JSON.parse(stringifiedDetailsOfPeople);
-    var li = document.createElement('li');
-    li.className="list-group-item";
-    li.id=user.email;
-    li.appendChild(document.createTextNode(`${user.name}: ${user.email}`));
-    var btn=document.createElement('button');
-    btn.className="btn btn-danger btn-sm float-right delete";
-    btn.appendChild(document.createTextNode('X'));
-    li.appendChild(btn);
-    btn=document.createElement('button');
-    btn.className="btn btn-primary btn-sm float-right edit";
-    btn.appendChild(document.createTextNode('-'));
-    li.appendChild(btn);
-    userList.appendChild(li);
-    
-    
-    
-    
-    });
 }
 
 function removeItem(e){
     e.preventDefault();
     if(e.target.classList.contains('delete')){
-
         var dele=e.target.parentElement.id;
-        localStorage.removeItem(dele);
+        axiosobj.delete(`/appointmentData/${dele}`)
+        .then(res=>console.log(res))
+        .catch(err=>console.log(err));
         e.target.parentElement.remove();
 
     }
